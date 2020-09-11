54990193ea63556902d4c60712e65154d7522e14
Susnigdha De <susnigdha.de@gmail.com>
2020-09-11 08:22:35 +0530

    Calculate Daily Employee Wage

---
+++ empWageV2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+#Calculate Daily Employee Wage
+
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
+if [ $isPresent -eq $randomCheck ];
+then
+	empRatePerHr=20;
+	empHrs=8;
+	salary=$(($empHrs*$empRatePerHr));
+else
+	salary=0;
+fi
+
